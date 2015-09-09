0596a020a3abe9c81a40eb8b9453da751cc94c06
ujwalp15 <ujwalp09@gmail.com>
2015-09-09 18:59:03 +0530

    Update

--- fusionsp.mk
+++ fusionsp.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/fusion/config/common_phone.mk)
